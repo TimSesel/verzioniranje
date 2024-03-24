ed4327c1b7eae828bebb62fcae7c8a58a5d31378
TimSesel <tim.sesel@student.um.si>
2024-03-24 13:09:23 +0100

    Gauss improved

--- linearni_operatorji.py
+++ linearni_operatorji.py
@@ -27,13 +27,17 @@ def konvolucija(slika, jedro):
     return nova_slika
 
 def filtriraj_z_gaussovim_jedrom(slika, sigma):
-    velikost_jedra = ((2 * sigma) * 2) + 1
+    velikost_jedra = (int)(((2 * sigma) * 2) + 1)
     k = (velikost_jedra / 2) - 0.5
     jedro = np.zeros((velikost_jedra, velikost_jedra), dtype=np.float32)
+    pod_enacba_1 = (1 / (2 * math.pi * (sigma ** 2))) * math.e
+    pod_enacba_2 = 2 * (sigma ** 2)
 
     for i in range(0, velikost_jedra):
+        pod_enacba_i = (i - k - 1) ** 2
         for j in range(0, velikost_jedra):
-            jedro[i, j] ((1 / (2 * math.pi * (sigma ** 2))) * math.e) ** (-((((i - k - 1) ** 2) + ((j - k - 1) ** 2)) / (2 * (sigma ** 2))))
+            pod_enacba_j = (j - k - 1) ** 2
+            jedro[i, j] = pod_enacba_1 ** (-(pod_enacba_i + pod_enacba_j) / pod_enacba_2)
 
     return konvolucija(slika, jedro)
 
@@ -46,7 +50,7 @@ if __name__ == '__main__':
     cv.waitKey(0)
 
     # --------------------------------- Konvolucija ---------------------------------
-    x = 0.1
+    x = 0.2
     y_jedro = 3
     x_jedro = 3
     jedro = np.full((y_jedro, x_jedro), x)
@@ -56,10 +60,10 @@ if __name__ == '__main__':
     cv.waitKey(0)
 
     # --------------------------------- Gauss ---------------------------------
-    sigma = 3.0
+    sigma = 2.0
     gauss_slika = filtriraj_z_gaussovim_jedrom(slika.copy().astype(np.float32) / 255.0, sigma)
 
-    cv.imshow("Konvolucija", konvolucirana_slika)
+    cv.imshow("Gauss", konvolucirana_slika)
     cv.waitKey(0)
 
     pass
