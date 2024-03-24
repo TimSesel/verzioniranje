af57079ef8933cce93a5f1ceab58f281d77b3e02
TimSesel <tim.sesel@student.um.si>
2024-03-24 10:02:12 +0100

    Updated python code with all needed functions

--- linearni_operatorji.py
+++ linearni_operatorji.py
@@ -1,3 +1,14 @@
-import numpy as np
 import cv2 as cv
+import numpy as np
 
+def konvolucija(slika, jedro):
+    pass
+
+def filtriraj_z_gaussovim_jedrom(slika, sigma):
+    pass
+
+def filtriraj_sobel_smer(slika):
+    pass
+
+if __name__ == '__main__':
+    pass
